b64c32c778a5c3bfc8f9cd46b2e9cd706d062901
HandidaT <htromep@gmail.com>
2026-03-01 09:30:01 -0800

    optimize isPrime

--- 1_0_4primenumber.cpp
+++ 1_0_4primenumber.cpp
@@ -6,15 +6,10 @@ using namespace std;
 
 bool isPrime(int n)
 {
-    // Corner case
-    if (n <= 1)
-        return false;
-  
-    // Check from 2 to n-1
-    for (int i = 2; i < n; i++)
+    if (n <= 1) return false;
+    for (int i = 2; i * i <= n; i++)
         if (n % i == 0)
             return false;
-  
     return true;
 }
 
