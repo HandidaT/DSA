4fffe9b58955de96b1b061ea36e6c0356d1cee7e
HandidaT <htromep@gmail.com>
2026-03-08 09:48:27 -0700

    hash table in c

--- 2_0_5hashtable.c
+++ 2_0_5hashtable.c
@@ -1,267 +1,295 @@
-#include<stdio.h>
-#include<stdlib.h>
-/*
-//Collision resolution by chaining, Singly linked list############################################################################################
-// Node for storing an item in a Linked List 
-typedef struct node {
-	int key;
-	int value;
-	struct node *next;
-} node;
+#include <stdio.h>
+#include <stdlib.h>
 
-typedef struct arrayitem {// For storing a Linked List at each index of Hash Table
-	node *head;// head pointing the first element of Linked List at an index of Hash Table
-	node *tail;// tail pointing the last element of Linked List at an index of Hash Table
-} arrayitem;
+#define INITIAL_SIZE 8
+#define LOAD_FACTOR 0.75
 
-arrayitem *array;
-int size = 0;         // Determines the no. of elements present in Hash Table
-int max = 10;	      // Determines the maximum capacity of Hash Table array
+/* ===============================
+   Hash Node
+   =============================== */
 
-int hashcode(int key){// This function creates an index corresponding to the every given key
-	return (key % max);
-}
-
-node* get_element(node *list, int key){//Returns node preceding to the desired node and NULL if desired node not present
-	node *temp = list, *tempprev=malloc(sizeof(*tempprev));
-	tempprev->next=temp;
-	while (temp != NULL){
-		if(temp->key==key) return tempprev;
-		tempprev=temp;
-		temp = temp->next;
-	}
-	free(tempprev);
-	return temp;//return NULL as element not present
-}
-
-void remove_element(int key){
-	int index=hashcode(key);
-	node *list=array[index].head;
-	if(list==NULL) printf("The key doesn't exist");
-	else{
-		node *element=get_element(list,key);
-		if(element==NULL) printf("The key doesn' exist");
-		else{
-			node *elementnext=element->next;
-			if(array[index].head==element->next){
-				array[index].head=element->next->next;
-				array[index].tail=element->next->next;//if element is head then it is also tail
-				printf("This key has been removed\n");
-			}
-			if(array[index].tail==element->next) array[index].head==element;
-			else element=element->next->next;
-			free(elementnext);
-			free(element);
-			printf("This key has been removed\n");
-		}
-	}
-}
-
-void init_array(){// For initializing the Hash Table
-	for(int i=0;i<max;i++){array[i].head=array[i].tail=NULL;}
-}
-
-void rehash();
-
-void insert(int key, int value){
-	float n=0.0;// n => Load Factor, keeps check on whether rehashing is required or not
-	int index=hashcode(key);
-	node *list=array[index].head;// Extracting Linked List at a given index
-	node *item=malloc(sizeof(*item));// Creating an item to insert in the Hash Table
-	item->key=key;
-	item->value=value;
-	item->next=NULL;
-	if(list==NULL){// Absence of Linked List at a given Index of Hash Table
-		printf("Inserting %d(key) and %d(value) \n", key, value);
-		array[index].head = array[index].tail = item;
-		size++;
-	}else{// A Linked List is present at given index of Hash Table
-		node *element=get_element(list, key);
-		if(element==NULL){//Key not found so adding the key at the end of the linked list
-			array[index].tail->next=item;
-			array[index].tail=item;
-			size++;
-		}else element->next->value=value;//Updating the value of already existing key
-	}
-	n=(1.0 *size) / max;//Calculating Load factor
-	if(n>=0.75){//rehashing
-		printf("going to rehash\n");
-		rehash();
-	}
-}
-
-void rehash(){
-	arrayitem *temp = array;//temp pointing to the current Hash Table array
-	int i = 0, n = max;
-	size = 0;
-	max = 2 * max;//array variable is assigned with newly created Hash Table
-	array = malloc(max * sizeof(*temp));//with double of previous array size
-	init_array();
-	for (i = 0; i < n; i++) {//Extracting the Linked List
- 		node* list = temp[i].head;//at position i of Hash Table array
-		if (list == NULL){ continue;//if there is no Linked List, then continue
-		}//Presence of Linked List at i Keep moving and accessing the
-                else {//Linked List item until the end. Get one key and value
-			while (list != NULL){//at a time and add it to new Hash Table array
-				insert(list->key, list->value);
-				list = list->next;
-			}
-		}
-	}
-	free(temp);
-}
-
-// Returns size of Hash Table
-int size_of_array(){
-	return size;
-}
- 
-// To display the contents of Hash Table
-void display(){
-	for (int i = 0; i < max; i++){
-		node *temp = array[i].head;
-		if (temp == NULL) printf("array[%d] has no elements\n", i);
-               else{
-			printf("array[%d] has elements-: ", i);
-			while (temp != NULL){
-				printf("key= %d  value= %d\t", temp->key, temp->value);
-				temp = temp->next;
-			}printf("\n");
-		}
-	}
-}*/
-
-
-//Open addressing, Linear probing####################################################################33
-struct HashNode {
+typedef struct {
     int key;
     int value;
-};
-  
-const int capacity = 20;
-int size = 0;
+    int status; 
+    // 0 = empty
+    // 1 = occupied
+    // 2 = deleted
+} HashNode;
 
-struct HashNode** arr;
-struct HashNode* dummy;
+/* ===============================
+   Hash Table Structure
+   =============================== */
 
-void insert_linearprobing(int key, int value){
-	struct HashNode *temp=malloc(sizeof(*temp));
-	temp->key=key;
-	temp->value=value;
-	int hashIndex=key%capacity;
-    // Find next free space
-	while(arr[hashIndex]!=NULL && arr[hashIndex]->key!=key && arr[hashIndex]->key!=-1){
-		hashIndex++;
-		hashIndex%=capacity;
-	}
-	if(arr[hashIndex]==NULL || arr[hashIndex]->key==-1) size++;
-	arr[hashIndex]=temp;
+typedef struct {
+    int size;
+    int capacity;
+    HashNode *table;
+} HashTable;
+
+/* ===============================
+   Hash Functions
+   =============================== */
+
+int hash1(int key, int capacity){
+    return key % capacity;
 }
-  
-// Function to delete a key value pair
-int delete (int key)
-{// Apply hash function to find
-    // index for given key
-    int hashIndex = key % capacity;
-    // Finding the node with given
-    // key
-    while (arr[hashIndex] != NULL) {
-        // if node found
-        if (arr[hashIndex]->key == key) {
-            // Insert dummy node here
-            // for further use
-            arr[hashIndex] = dummy;
-            // Reduce size
-            size--;
-            // Return the value of the key
-            return 1;
+
+int hash2(int key, int capacity){
+    return 1 + (key % (capacity - 1));
+}
+
+/* ===============================
+   Create Hash Table
+   =============================== */
+
+HashTable* create_table(){
+
+    HashTable *ht = malloc(sizeof(HashTable));
+
+    ht->size = 0;
+    ht->capacity = INITIAL_SIZE;
+
+    ht->table = malloc(sizeof(HashNode) * ht->capacity);
+
+    for(int i=0;i<ht->capacity;i++)
+        ht->table[i].status = 0;
+
+    return ht;
+}
+
+/* ===============================
+   Probing Strategies
+   =============================== */
+
+int linear_probe(int hash, int i, int capacity){
+    return (hash + i) % capacity;
+}
+
+int quadratic_probe(int hash, int i, int capacity){
+    return (hash + i*i) % capacity;
+}
+
+int double_hash_probe(int key, int i, int capacity){
+    return (hash1(key,capacity) + i * hash2(key,capacity)) % capacity;
+}
+
+/* ===============================
+   Rehashing
+   =============================== */
+
+void rehash(HashTable *ht){
+
+    int old_capacity = ht->capacity;
+    HashNode *old_table = ht->table;
+
+    ht->capacity = ht->capacity * 2;
+    ht->size = 0;
+
+    ht->table = malloc(sizeof(HashNode) * ht->capacity);
+
+    for(int i=0;i<ht->capacity;i++)
+        ht->table[i].status = 0;
+
+    for(int i=0;i<old_capacity;i++){
+
+        if(old_table[i].status == 1){
+
+            int key = old_table[i].key;
+            int value = old_table[i].value;
+
+            int hash = hash1(key, ht->capacity);
+            int j = 0;
+
+            while(1){
+
+                int index = linear_probe(hash,j,ht->capacity);
+
+                if(ht->table[index].status != 1){
+
+                    ht->table[index].key = key;
+                    ht->table[index].value = value;
+                    ht->table[index].status = 1;
+
+                    ht->size++;
+                    break;
+                }
+
+                j++;
+            }
         }
-        hashIndex++;
-        hashIndex %= capacity;
     }
-    // If not found return null
-    return 0;
+
+    free(old_table);
 }
-  
-// Function to search the value
-// for a given key
-int find(int key)
-{
-    // Apply hash function to find
-    // index for given key
-    int hashIndex = (key % capacity);
-  
-    int counter = 0;
-  
-    // Find the node with given key
-    while (arr[hashIndex] != NULL) {
-  
-        int counter = 0;
-        // If counter is greater than
-        // capacity
-        if (counter++ > capacity)
-            break;
-  
-        // If node found return its
-        // value
-        if (arr[hashIndex]->key == key)
-            return arr[hashIndex]->value;
-  
-        hashIndex++;
-        hashIndex %= capacity;
+
+/* ===============================
+   Insert
+   probe_type:
+   1 = Linear
+   2 = Quadratic
+   3 = Double Hashing
+   =============================== */
+
+void insert(HashTable *ht, int key, int value, int probe_type){
+
+    float lf = (float)ht->size / ht->capacity;
+
+    if(lf >= LOAD_FACTOR)
+        rehash(ht);
+
+    int hash = hash1(key, ht->capacity);
+
+    int i=0;
+
+    while(i < ht->capacity){
+
+        int index;
+
+        if(probe_type==1)
+            index = linear_probe(hash,i,ht->capacity);
+
+        else if(probe_type==2)
+            index = quadratic_probe(hash,i,ht->capacity);
+
+        else
+            index = double_hash_probe(key,i,ht->capacity);
+
+        if(ht->table[index].status != 1){
+
+            ht->table[index].key = key;
+            ht->table[index].value = value;
+            ht->table[index].status = 1;
+
+            ht->size++;
+            return;
+        }
+
+        i++;
     }
-  
-    // If not found return
-    // -1
+
+    printf("Insertion failed\n");
+}
+
+/* ===============================
+   Search
+   =============================== */
+
+int search(HashTable *ht, int key, int probe_type){
+
+    int hash = hash1(key, ht->capacity);
+
+    int i=0;
+
+    while(i < ht->capacity){
+
+        int index;
+
+        if(probe_type==1)
+            index = linear_probe(hash,i,ht->capacity);
+
+        else if(probe_type==2)
+            index = quadratic_probe(hash,i,ht->capacity);
+
+        else
+            index = double_hash_probe(key,i,ht->capacity);
+
+        if(ht->table[index].status == 0)
+            return -1;
+
+        if(ht->table[index].status==1 && ht->table[index].key==key)
+            return ht->table[index].value;
+
+        i++;
+    }
+
     return -1;
 }
- 
-void main() 
-{/*
-	//Collision resolution be chaining############################################################3
-	int choice, key, value, n, c;
-	array = malloc(max * sizeof(*array));
-	init_array();
-	insert(1, 5);
-	insert(0, 3);
-	insert(2, 6);
-	insert(3, 7);
-	display();
-	remove_element(2);
-	display();*/
 
-    //Open addressing, linear probing#####################################################################
-    // Space allocation
-    arr = malloc(sizeof(*arr) * capacity);
-    // Assign NULL initially
-    for (int i = 0; i < capacity; i++)
-        arr[i] = NULL;
-  
-    dummy = malloc(sizeof(*dummy));
-  
-    dummy->key = -1;
-    dummy->value = -1;
-  
-    insert_linearprobing(1, 5);
-    insert_linearprobing(2, 15);
-    insert_linearprobing(3, 20);
-    insert_linearprobing(4, 7);
-  
-    if (find(4) != -1)
-        printf("Value of Key 4 = %d\n", find(4));
-    else
-        printf("Key 4 does not exists\n");
-  
-    if (delete (4))
-        printf("Node value of key 4 is deleted "
-               "successfully\n");
-    else {
-        printf("Key does not exists\n");
+/* ===============================
+   Delete
+   =============================== */
+
+void delete_key(HashTable *ht, int key, int probe_type){
+
+    int hash = hash1(key, ht->capacity);
+
+    int i=0;
+
+    while(i < ht->capacity){
+
+        int index;
+
+        if(probe_type==1)
+            index = linear_probe(hash,i,ht->capacity);
+
+        else if(probe_type==2)
+            index = quadratic_probe(hash,i,ht->capacity);
+
+        else
+            index = double_hash_probe(key,i,ht->capacity);
+
+        if(ht->table[index].status == 0)
+            return;
+
+        if(ht->table[index].status==1 && ht->table[index].key==key){
+
+            ht->table[index].status = 2;
+            ht->size--;
+            printf("Key deleted\n");
+            return;
+        }
+
+        i++;
     }
-  
-    if (find(4) != -1)
-        printf("Value of Key 4 = %d\n", find(4));
-    else
-        printf("Key 4 does not exists\n");
+}
 
+/* ===============================
+   Display
+   =============================== */
+
+void display(HashTable *ht){
+
+    printf("\nHash Table\n");
+
+    for(int i=0;i<ht->capacity;i++){
+
+        if(ht->table[i].status == 1)
+            printf("%d -> (%d,%d)\n",i,ht->table[i].key,ht->table[i].value);
+
+        else
+            printf("%d -> NULL\n",i);
+    }
+
+}
+
+/* ===============================
+   MAIN
+   =============================== */
+
+int main(){
+
+    HashTable *ht = create_table();
+
+    int probe_type = 1; 
+    // 1 = Linear
+    // 2 = Quadratic
+    // 3 = Double Hashing
+
+    insert(ht,10,100,probe_type);
+    insert(ht,20,200,probe_type);
+    insert(ht,30,300,probe_type);
+    insert(ht,40,400,probe_type);
+    insert(ht,50,500,probe_type);
+    insert(ht,60,600,probe_type);
+
+    display(ht);
+
+    printf("\nSearch key 30: %d\n",search(ht,30,probe_type));
+
+    delete_key(ht,30,probe_type);
+
+    display(ht);
+
+    return 0;
 }
--- 2_0_5hashtable.cpp
+++ 2_0_5hashtable.cpp
@@ -1,244 +1,363 @@
 #include <iostream>
-#include <bits/stdc++.h>
-
+#include <vector>
+#include <list>
 using namespace std;
-/*//Direct address table ###################################################################
-class Directaddr_table{
-public:
-   int *T;
-   Directaddr_table(int n):T{new int[n]()}{};
 
-   void search(int key){
-     std::cout<<T[key]<<std::endl;
-   }
+#define INITIAL_CAPACITY 8
+#define LOAD_FACTOR 0.75
 
-   void insert(int key,int value){
-      T[key]=value;
-      std::cout<<"inserted";
-   }
+/* =========================================
+   HASH NODE
+   ========================================= */
 
-   void del(int key){
-      T[key]=0;
-   }
-
-   ~Directaddr_table(){delete[] T;}
-};*/
-
-//Open addressing, linear probing#######################################################################
-template <typename Ke, typename Va>// template for generic type
-
-class HashNode{// Hashnode class
-public:
-	Ke key;
-	Va value;
-	
-	HashNode(Ke key, Va value){
-		this->key=key;
-		this->value=value;
-	}
+struct Node {
+    int key;
+    int value;
 };
 
-template <typename K, typename V>// template for generic type
-class HashMap{
-	HashNode<K,V> **arr;// hash element array
-	HashNode<K,V> *dummy;// dummy node
-	int size;// current size
-	int capacity;
+/* =========================================
+   HASH TABLE CLASS
+   ========================================= */
+
+class HashTable {
+
+private:
+
+    int capacity;
+    int size;
+
+    vector<Node*> table;          // for open addressing
+    vector<list<Node>> chain;     // for separate chaining
+
 public:
-	HashMap(){
-		capacity=20;// Initial capacity of hash array
-		size=0;
-		arr=new HashNode<K,V>*[capacity];
-		dummy=new HashNode<K,V>(-1,-1);// dummy node with value and key -1
-		for(int i=0;i<capacity;i++){// Initialise all elements of array as NULL
-			arr[i]=NULL;
-		}
-	}
 
-	// This implements hash function to find index for a key
-	int hashCode(K key){ return key%capacity; }
+    /* =========================================
+       Constructor
+       ========================================= */
 
-	// Function to add key value pair
-	void insertNode(K key, V value)
-	{
-		HashNode<K, V>* temp = new HashNode<K, V>(key, value);
+    HashTable() {
 
-		// Apply hash function to find index for given key
-		int hashIndex = hashCode(key);
+        capacity = INITIAL_CAPACITY;
+        size = 0;
 
-		// find next free space
-		while (arr[hashIndex] != NULL
-			&& arr[hashIndex]->key != key
-			&& arr[hashIndex]->key != -1) {
-			hashIndex++;
-			hashIndex %= capacity;
-		}
-
-		// if new node to be inserted
-		// increase the current size
-		if (arr[hashIndex] == NULL
-			|| arr[hashIndex]->key == -1)
-			size++;
-		arr[hashIndex] = temp;
-	}
-
-	// Function to delete a key value pair
-	V deleteNode(int key)
-	{
-		// Apply hash function
-		// to find index for given key
-		int hashIndex = hashCode(key);
-
-		// finding the node with given key
-		while (arr[hashIndex] != NULL) {
-			// if node found
-			if (arr[hashIndex]->key == key) {
-				HashNode<K, V>* temp = arr[hashIndex];
-
-				// Insert dummy node here for further use
-				arr[hashIndex] = dummy;
-
-				// Reduce size
-				size--;
-				return temp->value;
-			}
-			hashIndex++;
-			hashIndex %= capacity;
-		}
-
-		// If not found return null
-		return NULL;
-	}
-
-	// Function to search the value for a given key
-	V get(int key)
-	{
-		// Apply hash function to find index for given key
-		int hashIndex = hashCode(key);
-		int counter = 0;
-
-		// finding the node with given key
-		while (arr[hashIndex] != NULL) { // int counter =0; // BUG!
-
-			if (counter++ > capacity) // to avoid infinite loop
-				return NULL;
-
-			// if node found return its value
-			if (arr[hashIndex]->key == key)
-				return arr[hashIndex]->value;
-			hashIndex++;
-			hashIndex %= capacity;
-		}
-
-		// If not found return null
-		return NULL;
-	}
-
-	// Return current size
-	int sizeofMap(){return size;}
-
-	// Return true if size is 0
-	bool isEmpty(){return size == 0;}
-
-	// Function to display the stored key value pairs
-	void display()
-	{
-		for (int i = 0; i < capacity; i++) {
-			if (arr[i] != NULL && arr[i]->key != -1)
-				cout << "key = " << arr[i]->key
-					<< " value = "
-					<< arr[i]->value << endl;
-		}
-	}
-};
-
-//Open addressing, Quadratic probing#######################################################################
-// Function to print an array
-void printArray(int arr[], int n)
-{
-    // Iterating and printing the array
-    for (int i = 0; i < n; i++)
-    {
-        cout << arr[i] << " ";
+        table.resize(capacity, nullptr);
+        chain.resize(capacity);
     }
-}
- 
-// Function to implement the
-// quadratic probing
-void hashing(int table[], int tsize,
-             int arr[], int N)
-{
-    // Iterating through the array
-    for (int i = 0; i < N; i++)
-    {
-        // Computing the hash value
-        int hv = arr[i] % tsize;
- 
-        // Insert in the table if there
-        // is no collision
-        if (table[hv] == -1)
-            table[hv] = arr[i];
-        else
-        {
-            // If there is a collision
-            // iterating through all
-            // possible quadratic values
-            for (int j = 0; j < tsize; j++)
-            {
-                // Computing the new hash value
-                int t = (hv + j * j) % tsize;
-                if (table[t] == -1)
-                {
-                    // Break the loop after
-                    // inserting the value
-                    // in the table
-                    table[t] = arr[i];
-                    break;
-                }
+
+    /* =========================================
+       Hash Functions
+       ========================================= */
+
+    int hash1(int key) {
+        return key % capacity;
+    }
+
+    int hash2(int key) {
+        return 1 + (key % (capacity - 1));
+    }
+
+    /* =========================================
+       Probing Methods
+       ========================================= */
+
+    int linearProbe(int hash, int i) {
+        return (hash + i) % capacity;
+    }
+
+    int quadraticProbe(int hash, int i) {
+        return (hash + i * i) % capacity;
+    }
+
+    int doubleHash(int key, int i) {
+        return (hash1(key) + i * hash2(key)) % capacity;
+    }
+
+    /* =========================================
+       Rehashing
+       ========================================= */
+
+    void rehash() {
+
+        int oldCapacity = capacity;
+        capacity = capacity * 2;
+
+        vector<Node*> oldTable = table;
+
+        table.clear();
+        table.resize(capacity, nullptr);
+
+        size = 0;
+
+        for (int i = 0; i < oldCapacity; i++) {
+
+            if (oldTable[i] != nullptr) {
+                insertLinear(oldTable[i]->key, oldTable[i]->value);
             }
         }
     }
-    printArray(table, N);
-}
 
+    /* =========================================
+       LOAD FACTOR CHECK
+       ========================================= */
 
+    void checkLoadFactor() {
 
+        float lf = (float)size / capacity;
 
-
-int main() {
-/*
-//Direct address table ###################################################################
-	Directaddr_table Dt(5);
-	Dt.insert(1,5);
-	Dt.search(1);*/
-
-//Open addressing, linear probing#######################################################################
-	HashMap<int, int>* h = new HashMap<int, int>;
-	h->insertNode(1, 1);
-	h->insertNode(2, 2);
-	h->insertNode(2, 3);
-	h->display();
-	cout << h->sizeofMap() << endl;
-	cout << h->deleteNode(2) << endl;
-	cout << h->sizeofMap() << endl;
-	cout << h->isEmpty() << endl;
-	cout << h->get(2);
-	
-//Open addressing, Quadratic probing#######################################################################
-    int arr[] = {50, 700, 76,
-                 85, 92, 73, 101};
-    int N = 7;
- 
-    // Size of the hash table
-    int L = 7;
-    int hash_table[7];
- 
-    // Initializing the hash table
-    for (int i = 0; i < L; i++)
-    {
-        hash_table[i] = -1;
+        if (lf >= LOAD_FACTOR)
+            rehash();
     }
- 
-    // Quadratic probing
-    hashing(hash_table, L, arr, N);
+
+    /* =========================================
+       INSERT (LINEAR PROBING)
+       ========================================= */
+
+    void insertLinear(int key, int value) {
+
+        checkLoadFactor();
+
+        int hash = hash1(key);
+
+        int i = 0;
+
+        while (i < capacity) {
+
+            int index = linearProbe(hash, i);
+
+            if (table[index] == nullptr) {
+
+                Node* node = new Node{key,value};
+                table[index] = node;
+
+                size++;
+                return;
+            }
+
+            i++;
+        }
+    }
+
+    /* =========================================
+       INSERT (QUADRATIC PROBING)
+       ========================================= */
+
+    void insertQuadratic(int key, int value) {
+
+        checkLoadFactor();
+
+        int hash = hash1(key);
+
+        int i = 0;
+
+        while (i < capacity) {
+
+            int index = quadraticProbe(hash, i);
+
+            if (table[index] == nullptr) {
+
+                Node* node = new Node{key,value};
+                table[index] = node;
+
+                size++;
+                return;
+            }
+
+            i++;
+        }
+    }
+
+    /* =========================================
+       INSERT (DOUBLE HASHING)
+       ========================================= */
+
+    void insertDoubleHash(int key, int value) {
+
+        checkLoadFactor();
+
+        int i = 0;
+
+        while (i < capacity) {
+
+            int index = doubleHash(key, i);
+
+            if (table[index] == nullptr) {
+
+                Node* node = new Node{key,value};
+                table[index] = node;
+
+                size++;
+                return;
+            }
+
+            i++;
+        }
+    }
+
+    /* =========================================
+       INSERT (SEPARATE CHAINING)
+       ========================================= */
+
+    void insertChaining(int key, int value) {
+
+        int index = hash1(key);
+
+        chain[index].push_back({key,value});
+    }
+
+    /* =========================================
+       SEARCH (OPEN ADDRESSING)
+       ========================================= */
+
+    int search(int key) {
+
+        int hash = hash1(key);
+
+        int i = 0;
+
+        while (i < capacity) {
+
+            int index = linearProbe(hash, i);
+
+            if (table[index] == nullptr)
+                return -1;
+
+            if (table[index]->key == key)
+                return table[index]->value;
+
+            i++;
+        }
+
+        return -1;
+    }
+
+    /* =========================================
+       SEARCH (CHAINING)
+       ========================================= */
+
+    int searchChaining(int key) {
+
+        int index = hash1(key);
+
+        for (auto &node : chain[index]) {
+
+            if (node.key == key)
+                return node.value;
+        }
+
+        return -1;
+    }
+
+    /* =========================================
+       DELETE (OPEN ADDRESSING)
+       ========================================= */
+
+    void deleteKey(int key) {
+
+        int hash = hash1(key);
+
+        int i = 0;
+
+        while (i < capacity) {
+
+            int index = linearProbe(hash, i);
+
+            if (table[index] == nullptr)
+                return;
+
+            if (table[index]->key == key) {
+
+                delete table[index];
+                table[index] = nullptr;
+
+                size--;
+                return;
+            }
+
+            i++;
+        }
+    }
+
+    /* =========================================
+       DELETE (CHAINING)
+       ========================================= */
+
+    void deleteChaining(int key) {
+
+        int index = hash1(key);
+
+        chain[index].remove_if([key](Node n){
+            return n.key == key;
+        });
+    }
+
+    /* =========================================
+       DISPLAY OPEN ADDRESSING
+       ========================================= */
+
+    void display() {
+
+        cout << "\nHash Table\n";
+
+        for(int i=0;i<capacity;i++){
+
+            if(table[i]!=nullptr)
+                cout<<i<<" -> ("<<table[i]->key<<","<<table[i]->value<<")\n";
+            else
+                cout<<i<<" -> NULL\n";
+        }
+    }
+
+    /* =========================================
+       DISPLAY CHAINING
+       ========================================= */
+
+    void displayChaining() {
+
+        cout<<"\nSeparate Chaining Table\n";
+
+        for(int i=0;i<capacity;i++){
+
+            cout<<i<<" : ";
+
+            for(auto &node : chain[i])
+                cout<<"("<<node.key<<","<<node.value<<") -> ";
+
+            cout<<"NULL\n";
+        }
+    }
+};
+
+
+/* =========================================
+   MAIN FUNCTION
+   ========================================= */
+
+int main(){
+
+    HashTable ht;
+
+    cout<<"Linear Probing\n";
+
+    ht.insertLinear(10,100);
+    ht.insertLinear(20,200);
+    ht.insertLinear(30,300);
+    ht.insertLinear(40,400);
+
+    ht.display();
+
+    cout<<"\nSearch 30 = "<<ht.search(30)<<endl;
+
+    ht.deleteKey(30);
+
+    ht.display();
+
+
+    cout<<"\nSeparate Chaining Example\n";
+
+    ht.insertChaining(1,10);
+    ht.insertChaining(11,20);
+    ht.insertChaining(21,30);
+
+    ht.displayChaining();
+
 }
