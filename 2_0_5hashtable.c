6a195ab4e8b1eb4f304618509fd6bfaf673110fa
HandidaT <htromep@gmail.com>
2026-04-11 02:00:56 -0700

    proper hashtable and binarysearch

--- 2_0_5hashtable.c
+++ 2_0_5hashtable.c
@@ -4,109 +4,104 @@
 #define INITIAL_SIZE 8
 #define LOAD_FACTOR 0.75
 
-/* ===============================
-   Hash Node
-   =============================== */
-
-typedef struct {
+typedef struct
+{
     int key;
     int value;
-    int status; 
-    // 0 = empty
-    // 1 = occupied
-    // 2 = deleted
+    int status;
 } HashNode;
 
-/* ===============================
-   Hash Table Structure
-   =============================== */
-
-typedef struct {
+typedef struct
+{
     int size;
     int capacity;
     HashNode *table;
 } HashTable;
 
-/* ===============================
-   Hash Functions
-   =============================== */
+enum ProbeType
+{
+    linear,
+    quadratic,
+    doubleHashing
+};
 
-int hash1(int key, int capacity){
+int hash1 (int key, int capacity)
+{
     return key % capacity;
 }
 
-int hash2(int key, int capacity){
-    return 1 + (key % (capacity - 1));
+int hash2 (int key, int capacity)
+{
+    return 1 + (key % (capacity-1));
 }
 
-/* ===============================
-   Create Hash Table
-   =============================== */
-
-HashTable* create_table(){
-
-    HashTable *ht = malloc(sizeof(HashTable));
-
+HashTable* create_table()
+{
+    HashTable *ht = malloc(sizeof(*ht));
     ht->size = 0;
     ht->capacity = INITIAL_SIZE;
 
-    ht->table = malloc(sizeof(HashNode) * ht->capacity);
+    ht->table = malloc(sizeof(*ht->table) * ht->capacity);
 
-    for(int i=0;i<ht->capacity;i++)
+    for (int i = 0; i < ht->capacity; i++)
         ht->table[i].status = 0;
 
     return ht;
 }
 
-/* ===============================
-   Probing Strategies
-   =============================== */
-
-int linear_probe(int hash, int i, int capacity){
+int linear_probing(int hash, int i, int capacity)
+{
     return (hash + i) % capacity;
 }
 
-int quadratic_probe(int hash, int i, int capacity){
-    return (hash + i*i) % capacity;
+int quadratic_probing(int hash, int i, int capacity)
+{
+    return (hash + i * i) % capacity;
 }
 
-int double_hash_probe(int key, int i, int capacity){
-    return (hash1(key,capacity) + i * hash2(key,capacity)) % capacity;
+int double_hashing(int key, int i, int capacity)
+{
+    return (hash1(key, capacity) + i * hash2(key, capacity)) % capacity;
 }
 
-/* ===============================
-   Rehashing
-   =============================== */
-
-void rehash(HashTable *ht){
-
+void rehash(HashTable *ht, int probe_type)
+{
     int old_capacity = ht->capacity;
     HashNode *old_table = ht->table;
 
-    ht->capacity = ht->capacity * 2;
+    ht->capacity = old_capacity * 2;
     ht->size = 0;
 
-    ht->table = malloc(sizeof(HashNode) * ht->capacity);
+    ht->table = malloc(sizeof(*ht->table) * ht->capacity);
 
-    for(int i=0;i<ht->capacity;i++)
+    for(int i = 0; i < ht->capacity; i++)
         ht->table[i].status = 0;
 
-    for(int i=0;i<old_capacity;i++){
-
-        if(old_table[i].status == 1){
-
+    for(int i = 0; i < old_capacity; i++)
+    {
+        if(old_table[i].status == 1)
+        {
             int key = old_table[i].key;
             int value = old_table[i].value;
-
             int hash = hash1(key, ht->capacity);
             int j = 0;
-
-            while(1){
-
-                int index = linear_probe(hash,j,ht->capacity);
-
-                if(ht->table[index].status != 1){
-
+            while(1)
+            {
+                int index;
+                switch(probe_type)
+                {
+                    case linear:
+                        index = linear_probing(hash, j, ht->capacity);
+                        break;
+                    case quadratic:
+                        index = quadratic_probing(hash, j, ht->capacity);
+                        break;
+                    case doubleHashing:
+                        index = double_hashing(key, j, ht->capacity);
+                        break;
+                }
+                if (ht->table[index].status != 1)
+                {
                     ht->table[index].key = key;
                     ht->table[index].value = value;
                     ht->table[index].status = 1;
@@ -114,7 +109,6 @@ void rehash(HashTable *ht){
                     ht->size++;
                     break;
                 }
-
                 j++;
             }
         }
@@ -123,40 +117,33 @@ void rehash(HashTable *ht){
     free(old_table);
 }
 
-/* ===============================
-   Insert
-   probe_type:
-   1 = Linear
-   2 = Quadratic
-   3 = Double Hashing
-   =============================== */
-
-void insert(HashTable *ht, int key, int value, int probe_type){
-
+void insert (HashTable *ht, int key, int value, int probe_type)
+{
     float lf = (float)ht->size / ht->capacity;
-
     if(lf >= LOAD_FACTOR)
-        rehash(ht);
-
+        rehash(ht, probe_type);
+    
     int hash = hash1(key, ht->capacity);
+    int i = 0;
 
-    int i=0;
-
-    while(i < ht->capacity){
-
+    while (i < ht->capacity)
+    {
         int index;
+        switch(probe_type)
+        {
+            case linear:
+                index = linear_probing(hash, i, ht->capacity);
+                break;
+            case quadratic:
+                index = quadratic_probing(hash, i, ht->capacity);
+                break;
+            case doubleHashing:
+                index = double_hashing(key, i, ht->capacity);
+                break;
+        }
 
-        if(probe_type==1)
-            index = linear_probe(hash,i,ht->capacity);
-
-        else if(probe_type==2)
-            index = quadratic_probe(hash,i,ht->capacity);
-
-        else
-            index = double_hash_probe(key,i,ht->capacity);
-
-        if(ht->table[index].status != 1){
-
+        if (ht->table[index].status == 0 || ht->table[index].status == 2)
+        {
             ht->table[index].key = key;
             ht->table[index].value = value;
             ht->table[index].status = 1;
@@ -164,79 +151,92 @@ void insert(HashTable *ht, int key, int value, int probe_type){
             ht->size++;
             return;
         }
-
         i++;
     }
 
-    printf("Insertion failed\n");
+    printf("\nInsertion failed\n");
 }
 
-/* ===============================
-   Search
-   =============================== */
+void update (HashTable *ht, int key, int value, int probe_type)
+{    
+    int hash = hash1(key, ht->capacity);
+    int i = 0;
 
-int search(HashTable *ht, int key, int probe_type){
+    while (i < ht->capacity)
+    {
+        int index;
+        switch(probe_type)
+        {
+            case linear:
+                index = linear_probing(hash, i, ht->capacity);
+                break;
+            case quadratic:
+                index = quadratic_probing(hash, i, ht->capacity);
+                break;
+            case doubleHashing:
+                index = double_hashing(key, i, ht->capacity);
+                break;
+        }
 
+        if(ht->table[index].status == 1 && ht->table[index].key == key)
+        {
+            ht->table[index].value = value;
+            return;
+        }
+        i++;
+    }
+
+    printf("\nUpdate failed\n");
+}
+
+int search (HashTable *ht, int key, int probe_type)
+{
     int hash = hash1(key, ht->capacity);
 
-    int i=0;
+    int i = 0;
 
-    while(i < ht->capacity){
-
+    while (i < ht->capacity)
+    {
         int index;
 
-        if(probe_type==1)
-            index = linear_probe(hash,i,ht->capacity);
-
-        else if(probe_type==2)
-            index = quadratic_probe(hash,i,ht->capacity);
-
+        if (probe_type == linear)
+            index = linear_probing(hash, i, ht->capacity);
+        else if (probe_type == quadratic)
+            index = quadratic_probing(hash, i, ht->capacity);
         else
-            index = double_hash_probe(key,i,ht->capacity);
+            index = double_hashing(key, i, ht->capacity);
 
         if(ht->table[index].status == 0)
             return -1;
 
-        if(ht->table[index].status==1 && ht->table[index].key==key)
+        if(ht->table[index].status == 1 && ht->table[index].key == key)
             return ht->table[index].value;
-
+        
         i++;
     }
 
     return -1;
 }
 
-/* ===============================
-   Delete
-   =============================== */
-
-void delete_key(HashTable *ht, int key, int probe_type){
-
+void delete_key (HashTable *ht, int key, int probe_type)
+{
     int hash = hash1(key, ht->capacity);
-
     int i=0;
 
-    while(i < ht->capacity){
-
+    while(i < ht->capacity)
+    {
         int index;
-
-        if(probe_type==1)
-            index = linear_probe(hash,i,ht->capacity);
-
-        else if(probe_type==2)
-            index = quadratic_probe(hash,i,ht->capacity);
-
+        if(probe_type == linear)
+            index = linear_probing(hash, i, ht->capacity);
+        else if(probe_type == quadratic)
+            index = quadratic_probing(hash, i, ht->capacity);
         else
-            index = double_hash_probe(key,i,ht->capacity);
+            index = double_hashing(key, i, ht->capacity);
 
-        if(ht->table[index].status == 0)
-            return;
-
-        if(ht->table[index].status==1 && ht->table[index].key==key){
-
+        if (ht->table[index].status == 1 && ht->table[index].key==key)
+        {
             ht->table[index].status = 2;
             ht->size--;
-            printf("Key deleted\n");
             return;
         }
 
@@ -263,11 +263,159 @@ void display(HashTable *ht){
 
 }
 
+/* =====================================================
+   SEPARATE CHAINING HASH TABLE
+   ===================================================== */
+
+typedef struct ChainNode {
+    int key;
+    int value;
+    struct ChainNode *next;
+} ChainNode;
+
+typedef struct {
+    int capacity;
+    int size;
+    ChainNode **table;
+} ChainHashTable;
+
+
 /* ===============================
-   MAIN
+   Create Chaining Table
    =============================== */
 
-int main(){
+ChainHashTable* create_chain_table(int capacity){
+
+    ChainHashTable *cht = malloc(sizeof(*cht));
+
+    cht->capacity = capacity;
+    cht->size = 0;
+
+    cht->table = malloc(sizeof(ChainNode*) * capacity);
+
+    for(int i=0;i<capacity;i++)
+        cht->table[i] = NULL;
+
+    return cht;
+}
+
+
+/* ===============================
+   Insert (Chaining)
+   =============================== */
+
+void insert_chain(ChainHashTable *cht, int key, int value){
+
+    int index = hash1(key, cht->capacity);
+
+    ChainNode *newNode = malloc(sizeof(*newNode));
+    newNode->key = key;
+    newNode->value = value;
+    newNode->next = NULL;
+
+    if(cht->table[index] == NULL){
+        cht->table[index] = newNode;
+    } else {
+        ChainNode *temp = cht->table[index];
+
+        // update if key exists
+        while(temp != NULL){
+            if(temp->key == key){
+                temp->value = value;
+                free(newNode);
+                return;
+            }
+            if(temp->next == NULL) break;
+            temp = temp->next;
+        }
+
+        temp->next = newNode;
+    }
+
+    cht->size++;
+}
+
+
+/* ===============================
+   Search (Chaining)
+   =============================== */
+
+int search_chain(ChainHashTable *cht, int key){
+
+    int index = hash1(key, cht->capacity);
+
+    ChainNode *temp = cht->table[index];
+
+    while(temp){
+        if(temp->key == key)
+            return temp->value;
+        temp = temp->next;
+    }
+
+    return -1;
+}
+
+
+/* ===============================
+   Delete (Chaining)
+   =============================== */
+
+void delete_chain(ChainHashTable *cht, int key){
+
+    int index = hash1(key, cht->capacity);
+
+    ChainNode *temp = cht->table[index];
+    ChainNode *prev = NULL;
+
+    while(temp){
+
+        if(temp->key == key){
+
+            if(prev == NULL)
+                cht->table[index] = temp->next;
+            else
+                prev->next = temp->next;
+
+            free(temp);
+            cht->size--;
+
+            printf("Key deleted (chaining)\n");
+            return;
+        }
+
+        prev = temp;
+        temp = temp->next;
+    }
+
+    printf("Key not found (chaining)\n");
+}
+
+
+/* ===============================
+   Display (Chaining)
+   =============================== */
+
+void display_chain(ChainHashTable *cht){
+
+    printf("\nChaining Hash Table\n");
+
+    for(int i=0;i<cht->capacity;i++){
+
+        printf("%d -> ", i);
+
+        ChainNode *temp = cht->table[i];
+
+        while(temp){
+            printf("(%d,%d) -> ", temp->key, temp->value);
+            temp = temp->next;
+        }
+
+        printf("NULL\n");
+    }
+}
+
+int main()
+{
 
     HashTable *ht = create_table();
 
@@ -290,6 +438,22 @@ int main(){
     delete_key(ht,30,probe_type);
 
     display(ht);
+
+    printf("\n\n===== SEPARATE CHAINING =====\n");
+    
+    ChainHashTable *cht = create_chain_table(8);
+    
+    insert_chain(cht, 10, 100);
+    insert_chain(cht, 18, 180);  // collision
+    insert_chain(cht, 26, 260);  // collision
+    
+    display_chain(cht);
+    
+    printf("\nSearch 18 = %d\n", search_chain(cht, 18));
+    
+    delete_chain(cht, 18);
+    
+    display_chain(cht);
 
     return 0;
 }
--- 2_0_6binarysearchtree.c
+++ 2_0_6binarysearchtree.c
@@ -1,114 +1,225 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// Define a structure for a binary tree node
-struct Node
-{
+// // Define a structure for a binary tree node
+// struct Node
+// {
+//     int key;
+//     struct Node *left, *right;
+// };
+
+// // Function to create a new node with a given value
+// struct Node *newNodeCreate(int value)
+// {
+//     struct Node *temp = (struct Node *)malloc(sizeof(struct Node));
+//     temp->key = value;
+//     temp->left = temp->right = NULL;
+//     return temp;
+// }
+
+// // Function to search for a node with a specific key in the tree
+// struct Node *searchNode(struct Node *root, int target)
+// {
+//     if (root == NULL || root->key == target)
+//         return root;
+//     if (root->key < target)
+//         return searchNode(root->right, target);
+//     return searchNode(root->left, target);
+// }
+
+// // Function to insert a node with a specific value in the tree
+// struct Node *insertNode(struct Node *node, int value)
+// {
+//     if (node == NULL)
+//         return newNodeCreate(value);
+//     if (value < node->key)
+//         node->left = insertNode(node->left, value);
+//     else if (value > node->key)
+//         node->right = insertNode(node->right, value);
+//     return node;
+// }
+
+// // Function to perform post-order traversal
+// void postOrder(struct Node *root)
+// {
+//     if (root != NULL)
+//     {
+//         postOrder(root->left);
+//         postOrder(root->right);
+//         printf(" %d ", root->key);
+//     }
+// }
+
+// // Function to perform in-order traversal
+// void inOrder(struct Node *root)
+// {
+//     if (root != NULL)
+//     {
+//         inOrder(root->left);
+//         printf(" %d ", root->key);
+//         inOrder(root->right);
+//     }
+// }
+
+// // Function to perform pre-order traversal
+// void preOrder(struct Node *root)
+// {
+//     if (root != NULL)
+//     {
+//         printf(" %d ", root->key);
+//         preOrder(root->left);
+//         preOrder(root->right);
+//     }
+// }
+
+// // Function to find the minimum value
+// struct Node *findMin(struct Node *root)
+// {
+//     if (root == NULL)
+//         return NULL;
+//     else if (root->left != NULL)
+//         return findMin(root->left);
+//     return root;
+// }
+
+// // Function to delete a node from the tree
+// struct Node *delete (struct Node *root, int x)
+// {
+//     if (root == NULL)
+//         return NULL;
+//     if (x > root->key)
+//         root->right = delete (root->right, x);
+//     else if (x < root->key)
+//         root->left = delete (root->left, x);
+//     else
+//     {
+//         if (root->left == NULL && root->right == NULL)
+//         {
+//             free(root);
+//             return NULL;
+//         }
+//         else if (root->left == NULL || root->right == NULL)
+//         {
+//             struct Node *temp;
+//             if (root->left == NULL)
+//             {
+//                 temp = root->right;
+//             }
+//             else
+//             {
+//                 temp = root->left;
+//             }
+//             free(root);
+//             return temp;
+//         }
+//         else
+//         {
+//             struct Node *temp = findMin(root->right);
+//             root->key = temp->key;
+//             root->right = delete (root->right, temp->key);
+//         }
+//     }
+//     return root;
+// }
+
+
+struct Node {
     int key;
     struct Node *left, *right;
 };
 
-// Function to create a new node with a given value
-struct Node *newNodeCreate(int value)
+struct Node* newNodeCreate(int value)
 {
-    struct Node *temp = (struct Node *)malloc(sizeof(struct Node));
-    temp->key = value;
-    temp->left = temp->right = NULL;
-    return temp;
+    struct Node* newNode = malloc(sizeof(*newNode));
+    newNode->key = value;
+    newNode->left = newNode->right = NULL;
+
+    return newNode;
 }
 
-// Function to search for a node with a specific key in the tree
-struct Node *searchNode(struct Node *root, int target)
+struct Node* searchNode (struct Node* root, int key)
 {
-    if (root == NULL || root->key == target)
-        return root;
-    if (root->key < target)
-        return searchNode(root->right, target);
-    return searchNode(root->left, target);
+    if (root == NULL || root->key == key)
+      return root;
+    
+    if (root->key < key)
+        return searchNode(root->right, key);
+
+    return searchNode(root->left, key);
 }
 
-// Function to insert a node with a specific value in the tree
-struct Node *insertNode(struct Node *node, int value)
+struct Node* insertNode (struct Node* root, int value)
 {
-    if (node == NULL)
+    if (root == NULL)
         return newNodeCreate(value);
-    if (value < node->key)
-        node->left = insertNode(node->left, value);
-    else if (value > node->key)
-        node->right = insertNode(node->right, value);
-    return node;
+    
+    if(value < root->key)
+        root->left = insertNode(root->left, value);
+    else if(value > root->key)
+        root->right = insertNode(root->right, value);
+    
+    return root;
 }
 
-// Function to perform post-order traversal
-void postOrder(struct Node *root)
+void preOrder (struct Node* root)
 {
     if (root != NULL)
     {
-        postOrder(root->left);
-        postOrder(root->right);
-        printf(" %d ", root->key);
-    }
-}
-
-// Function to perform in-order traversal
-void inOrder(struct Node *root)
-{
-    if (root != NULL)
-    {
-        inOrder(root->left);
-        printf(" %d ", root->key);
-        inOrder(root->right);
-    }
-}
-
-// Function to perform pre-order traversal
-void preOrder(struct Node *root)
-{
-    if (root != NULL)
-    {
-        printf(" %d ", root->key);
+        printf("\n Key %d\n", root->key);
         preOrder(root->left);
         preOrder(root->right);
     }
 }
 
-// Function to find the minimum value
-struct Node *findMin(struct Node *root)
+void inOrder (struct Node* root)
 {
-    if (root == NULL)
-        return NULL;
-    else if (root->left != NULL)
-        return findMin(root->left);
-    return root;
+    if (root != NULL)
+    {
+        inOrder(root->left);
+        printf("\n %d \n", root->key);
+        inOrder(root->right);
+    }
 }
 
-// Function to delete a node from the tree
-struct Node *delete (struct Node *root, int x)
+void postOrder (struct Node* root)
+{
+    if (root != NULL)
+    {
+        postOrder(root->left);
+        postOrder(root->right);
+        printf("\n %d \n", root->key);
+    }
+}
+
+struct Node *findMin(struct Node* node)
+{
+    while (node && node->left != NULL)
+        node = node->left;
+    return node;
+}
+
+struct Node *deleteNode (struct Node *root, int x)
 {
     if (root == NULL)
         return NULL;
     if (x > root->key)
-        root->right = delete (root->right, x);
+        root->right = deleteNode(root->right, x);
     else if (x < root->key)
-        root->left = delete (root->left, x);
+        root->left = deleteNode(root->left, x);
     else
     {
-        if (root->left == NULL && root->right == NULL)
-        {
+        if (root->left == NULL && root->right == NULL){
             free(root);
             return NULL;
         }
         else if (root->left == NULL || root->right == NULL)
         {
             struct Node *temp;
-            if (root->left == NULL)
-            {
+            if(root->left == NULL)
                 temp = root->right;
-            }
             else
-            {
                 temp = root->left;
-            }
+            
             free(root);
             return temp;
         }
@@ -116,7 +227,7 @@ struct Node *delete (struct Node *root, int x)
         {
             struct Node *temp = findMin(root->right);
             root->key = temp->key;
-            root->right = delete (root->right, temp->key);
+            root->right = deleteNode (root->right, temp->key);
         }
     }
     return root;
@@ -157,7 +268,7 @@ int main()
     printf("\n");
 
     // Perform delete the node (70)
-    struct Node *temp = delete (root, 70);
+    struct Node *temp = deleteNode (root, 70);
     printf("After Delete: \n");
     inOrder(root);
 
