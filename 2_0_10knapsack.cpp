7fb3fd878ccd6abf7407b128492dea8bb9f5dbfa
HandidaT <htromep@gmail.com>
2026-04-12 06:37:51 -0700

    01knapsack

--- 2_0_10knapsack.cpp
+++ 2_0_10knapsack.cpp
@@ -1,125 +1,94 @@
-// Note that by default C++ creates a max-heap
-// for priority queue
 #include <iostream>
 #include <vector>
-#include <queue>
- 
+using namespace std;
 
-void showpq(std::priority_queue<int> gq)
-{
-    std::priority_queue<int> g = gq;
-    while (!g.empty()) {
-        std::cout << '\t' << g.top();
-        g.pop();
+int knapsackRec(int W, vector<int> &val, vector<int> &wt, int n, 
+                                        vector<vector<int>> &memo) {
+
+    // Base Case
+    if (n == 0 || W == 0)
+        return 0;
+
+    // Check if we have previously calculated the same subproblem
+    if(memo[n][W] != -1)
+        return memo[n][W];
+
+    int pick = 0;
+
+    // Pick nth item if it does not exceed the capacity of knapsack
+    if (wt[n - 1] <= W)
+        pick = val[n - 1] + knapsackRec(W - wt[n - 1], val, wt, n - 1, memo);
+    
+    // Don't pick the nth item
+    int notPick = knapsackRec(W, val, wt, n - 1, memo);
+    
+    // Store the result in memo[n][W] and return it
+    return memo[n][W] = max(pick, notPick);
+}
+
+int knapsackMemoization(int W, vector<int> &val, vector<int> &wt) {
+    int n = val.size();
+    
+    // Memoization table to store the results
+    vector<vector<int>> memo(n + 1, vector<int>(W + 1, -1));
+    
+    return knapsackRec(W, val, wt, n, memo);
+}
+
+int knapsackTabulation(int W, vector<int> &val, vector<int> &wt) {
+    int n = wt.size();
+    vector<vector<int>> dp(n + 1, vector<int>(W + 1));
+
+    // Build table dp[][] in bottom-up manner
+    for (int i = 0; i <= n; i++) {
+        for (int j = 0; j <= W; j++) {
+            
+            // If there is no item or the knapsack's capacity is 0
+            if (i == 0 || j == 0)
+                dp[i][j] = 0;
+            else {
+                int pick = 0;
+                
+                // Pick ith item if it does not exceed the capacity of knapsack
+                if(wt[i - 1] <= j)
+                    pick = val[i - 1] + dp[i - 1][j - wt[i - 1]];
+                    
+                // Don't pick the ith item
+                int notPick = dp[i - 1][j];
+                
+                dp[i][j] = max(pick, notPick);
+            }
+        }
     }
-    std::cout << '\n';
+    return dp[n][W];
 }
 
-// Priority Queue implementation in C++
+//Driver Code Ends
+int knapsackTabulationSpaceOptimized(int W, vector<int> &val, vector<int> &wt) {
 
+    // Initializing dp vector
+    vector<int> dp(W + 1, 0);
 
-
-// Function to swap position of two elements
-void swap(int *a, int *b) {
-  int temp = *b;
-  *b = *a;
-  *a = temp;
-}
-
-int parent(int i) {return (i-1)/2;}
-
-// Function to heapify the tree
-void heapify(std::vector<int> &hT, int i) {
-  int size = hT.size();
-  
-  // Find the largest among root, left child and right child
-  int largest = i;
-  int l = 2 * i + 1;
-  int r = 2 * i + 2;
-  if (l < size && hT[l] > hT[i])
-    largest = l;
-  if (r < size && hT[r] > hT[largest])
-    largest = r;
-
-  // Swap and continue heapifying if root is not largest
-  if (largest != i) {
-    swap(&hT[i], &hT[largest]);
-    heapify(hT, largest);
-  }
-}
-
-// Function to insert an element into the tree
-void insert(std::vector<int> &hT, int newNum) {
-  int size = hT.size();
-  if (size == 0) {  hT.push_back(newNum);
-  } else {
-    hT.push_back(newNum);
-    //for (int i = size / 2 - 1; i >= 0; i--) {
-    //heapify(hT, i);
-    while(size!=0 && hT[parent(size)]<hT[size]){
-    	swap(&hT[size], &hT[parent(size)]);
-    	size=parent(size);
+    // Taking first i elements
+    for (int i = 1; i <= wt.size(); i++) {
+        
+        // Starting from back, so that we also have data of
+        // previous computation of i-1 items
+        for (int j = W; j >= wt[i - 1]; j--) {
+            dp[j] = max(dp[j], dp[j - wt[i - 1]] + val[i - 1]);
+        }
     }
-  }
+    return dp[W];
 }
 
-// Function to delete an element from the tree
-void deleteNode(std::vector<int> &hT, int num) {
-  int size = hT.size();
-  int i;
-  for (i = 0; i < size; i++) {
-    if (num == hT[i])
-      break;
-  }
-  swap(&hT[i], &hT[size - 1]);
+int main() {
+//Driver Code Starts
+    vector<int> val = {1, 2, 3};
+    vector<int> wt = {4, 5, 1};
+    int W = 4;
 
-  hT.pop_back();
-  for (int i = size / 2 - 1; i >= 0; i--) {
-    heapify(hT, i);
-  }
-}
-
-// Print the tree
-void printArray(std::vector<int> &hT) {
-  for (int i = 0; i < hT.size(); ++i)
-    std::cout << hT[i] << " ";
-  std::cout << "\n";
-}
- 
-int main()
-{
-    std::priority_queue<int> gquiz;
-    gquiz.push(3);
-    gquiz.push(4);
-    gquiz.push(9);
-    gquiz.push(5);
-    gquiz.push(2);
- 
-    std::cout << "The priority queue gquiz is : ";
-    showpq(gquiz);
- 
-    std::cout << "\ngquiz.size() : " << gquiz.size();
-    std::cout << "\ngquiz.top() : " << gquiz.top();
- 
-    std::cout << "\ngquiz.pop() : ";
-    gquiz.pop();
-    showpq(gquiz);
-
-/*
-  vector<int> heapTree;
-
-  insert(heapTree, 3);
-  insert(heapTree, 4);
-  insert(heapTree, 9);
-  insert(heapTree, 5);
-  insert(heapTree, 2);
-
-  cout << "Max-Heap array: ";
-  printArray(heapTree);
-
-  deleteNode(heapTree, 4);
-
-  cout << "After deleting an element: ";
-
-  printArray(heapTree);*/
+    cout << knapsackMemoization(W, val, wt) << endl;
+    cout << knapsackTabulation(W, val, wt) << endl;
+    cout << knapsackTabulationSpaceOptimized(W, val, wt) << endl;
+    return 0;
 }
